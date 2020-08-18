2694fed0e9b9a4822428505bdc2d2d98c59479a6
Ed Summers <ehs@pobox.com>
2020-08-18 12:59:32 -0400

    new description + version

--- setup.py
+++ setup.py
@@ -1,23 +1,19 @@
 from setuptools import setup
 
-description = \
-"""
-ptree is a minimal implementation of PairTree, which is a filesystem
-convention from the digital preservation community for mapping
-identifiers to file system locations, and vice versa.
-
-See https://confluence.ucop.edu/display/Curation/PairTree for more details.
-"""
+with open("README.md") as f:
+    long_description = f.read()
 
 setup(
     name = 'ptree',
-    version = '0.1',
+    version = '0.2',
     url = 'http://github.com/edsu/ptree',
     author = 'Ed Summers',
     author_email = 'ehs@pobox.com',
     py_modules = ['ptree',],
     scripts = ['ptree.py'],
-    description = description,
+    description = 'Work with PairTree file system convention',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     platforms = ['POSIX'],
     test_suite = 'test',
     classifiers = [
